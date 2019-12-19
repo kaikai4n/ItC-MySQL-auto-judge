e8ccc0e31f5e444c7566350bb21e29351df10e4f
kaikai4n <b04902131@ntu.edu.tw>
2019-12-19 16:46:10 +0800

    Add auto judge to check score of each students.'

--- judge.py
+++ judge.py
@@ -1,9 +1,11 @@
 import argparse
 import os
 import requests
+import sys
 import zipfile
-from urllib.parse import urlparse
 from pathlib import Path
+from time import sleep
+from urllib.parse import urlparse
 
 
 def get_args():
@@ -23,12 +25,23 @@ def get_args():
         default='./files',
         help='The unzip file save directory name'
     )
+    parser.add_argument(
+        '--output',
+        default='score.csv',
+        help='The output score csv file.'
+    )
     args = parser.parse_args()
     return args
 
 
 class Error:
+    class PaizaParserError(Exception):
+        pass
+
     class ParseAnswerError(Exception):
+        pass
+            
+    class AnswerCheckerError(Exception):
         pass
 
 
@@ -41,6 +54,18 @@ class PaizaParser:
     def get_answer_url(cls, url):
         answer_url = f'https://out.paiza.io{cls.get_hash(url)}/output.txt'
         return answer_url
+
+    @classmethod
+    def get_answer(cls, url):
+        res = requests.get(cls.get_answer_url(url))
+        sleep(0.5)
+        if res.status_code != 200:
+            raise Error.PaizaParserError(
+                f'Possible wrong link is given "{url}" '
+                f'with status code {res.status_code}.')
+        answer_text = res.text
+        answer = AnswerParser.parse_answer_text(answer_text)
+        return answer
 
 
 class AnswerParser:
@@ -68,7 +93,8 @@ class AnswerParser:
                 continue
             answers[q_count - 1].add(next_line)
         if q_count != total_queries:
-            raise Error.ParseAnswerError(f'Get {q_count} queries')
+            raise Error.ParseAnswerError(
+                f'Possible wrong output format. Get {q_count} queries')
         return answers
 
 
@@ -87,12 +113,78 @@ class DataProcessor:
                 zip_ref.extractall(self.sp)
 
 
+class AnswerChecker:
+    def __init__(self, root_path, save_csv, answer_link, answer_func):
+        self.p = root_path
+        self.sp = save_csv
+        self.answer = answer_func(answer_link)
+        self.answer_func = answer_func
+
+    @staticmethod
+    def parse_url_from_file(fn):
+        try:
+            with open(fn, 'r') as f:
+                return f.readline().strip()
+        except:
+            raise Error.AnswerCheckerError(f'Cannot open file {fn}')
+                        
+    def parse_answer_from_file(self, fn):
+        try:
+            with open(fn, 'r') as f:
+                content = f.read()
+
+            answer = AnswerParser.parse_answer_text(content)
+            return answer
+        except:
+            raise Error.AnswerCheckerError(f'Cannot open file {fn}')
+
+    @staticmethod
+    def check_answer_score(correct_answer, answer, fn_answer, accum=20):
+        total_score = 0
+        for q_1, q_2, q_3 in zip(correct_answer, answer, fn_answer):
+            if q_1 == q_2 == q_3:
+                total_score += accum
+        return total_score
+
+    def check(self):
+        p = Path(self.p)
+        with open(self.sp, 'w') as f:
+            for dir_name in p.glob('*'):
+                if dir_name.is_dir():
+                    name = dir_name.name
+                    score = 0
+                    print(f'Processing {name}', file=sys.stderr)
+                    try:
+                        url_fn = dir_name.joinpath(dir_name.name + '.txt')
+                        url = self.parse_url_from_file(url_fn)
+                        out_fn = dir_name.joinpath('output.txt')
+                        fn_answer = self.parse_answer_from_file(out_fn)
+                        answer = self.answer_func(url)
+                        score += self.check_answer_score(
+                            self.answer, answer, fn_answer)
+                        self.write_to_csv(
+                            f, name, score, '')
+                    except Error.PaizaParserError as e:
+                        self.write_to_csv(f, name, score, e)
+                    except Error.AnswerCheckerError as e:
+                        self.write_to_csv(f, name, score, e)
+                    except Error.ParseAnswerError as e:
+                        self.write_to_csv(f, name, score, e)
+    
+    @staticmethod
+    def write_to_csv(f, name, score, msg):
+        out = f'{name},{score},{msg}\n'
+        f.write(out)
+
+
 if __name__ == '__main__':
     args = get_args()
-    answer_text = requests.get(PaizaParser.get_answer_url(args.answer_link)).text
-    answer = AnswerParser.parse_answer_text(answer_text)
     dp = DataProcessor(args.data_dir, args.unzip_dir)
     dp.unzip_files()
-    import pdb
-    pdb.set_trace()
-    pass
+    ac = AnswerChecker(
+        args.unzip_dir,
+        args.output,
+        args.answer_link,
+        PaizaParser.get_answer
+    )
+    ac.check()
