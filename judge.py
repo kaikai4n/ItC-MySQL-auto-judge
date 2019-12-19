434be4512663fa5852c8ad7e0dbf7f72b6d6f748
kaikai4n <b04902131@ntu.edu.tw>
2019-12-19 14:47:24 +0800

    Add required args.

--- judge.py
+++ judge.py
@@ -5,13 +5,17 @@ def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument(
         '--data-dir',
+        required=True,
         help='Put zip files from ceiba in the directory'
     )
     parser.add_argument(
         '--answer-link',
+        required=True,
         help='TA reference answer link.'
     )
     args = parser.parse_args()
     return args
 
-print(get_args())
+
+if __name__ == '__main__':
+    args = get_args()
