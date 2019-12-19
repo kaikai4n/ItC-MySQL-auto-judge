77483a69c71787245f784d8f3af32a4c64df7e9f
kaikai4n <b04902131@ntu.edu.tw>
2019-12-19 15:30:45 +0800

    Parse answer from website output.

--- judge.py
+++ judge.py
@@ -19,6 +19,11 @@ def get_args():
     return args
 
 
+class Error:
+    class ParseAnswerError(Exception):
+        pass
+
+
 class PaizaParser:
     @staticmethod
     def get_hash(url):
@@ -30,6 +35,39 @@ class PaizaParser:
         return answer_url
 
 
+class AnswerParser:
+    @staticmethod
+    def get_line_gen(text):
+        for one_text in text.split('\n'):
+            yield one_text
+
+    @classmethod
+    def parse_answer_text(cls, text, total_queries=5):
+        answers = [set() for _ in range(total_queries)]
+        text_gen = cls.get_line_gen(text)
+        q_count = 0
+        while True:
+            next_q = f'Q{q_count + 1}:'
+            try:
+                next_line = next(text_gen)
+            except StopIteration:
+                break
+            next_line = next_line.strip()
+            if not next_line:
+                continue
+            if next_q == next_line:
+                q_count += 1
+                continue
+            answers[q_count - 1].add(next_line)
+        if q_count != total_queries:
+            raise Error.ParseAnswerError(f'Get {q_count} queries')
+        return answers
+
+
 if __name__ == '__main__':
     args = get_args()
     answer_text = requests.get(PaizaParser.get_answer_url(args.answer_link)).text
+    answer = AnswerParser.parse_answer_text(answer_text)
+    import pdb
+    pdb.set_trace()
+    pass
